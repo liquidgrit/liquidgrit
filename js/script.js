2b1b3ad4624a9707f74ca654138211233fe108e1
liquidgrit <brett.nowak@liquidandgrit.com>
2016-07-31 10:03:19 -0700

    change to skeleton

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+  function myFunction() {
+      document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+  }
+
+});
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$
+(document).ready(function(){
+
+  function myFunction() {
+      document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+  }
+
+});
